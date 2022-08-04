562a160b281eab264bd6d43a7284afd73b90a1cf
Kholed Langsari <langsaree@gmail.com>
2022-08-04 13:25:53 +0700

    php oop + workshop

---
+++ 6-php-function-oop/php-oop/accessing-properties-and-methods.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * LangsariSoftware - A Delicious PHP Component
+ *
+ * @package  LangsariEngine
+ * @author   Kholed Langsari <langsaree@gmail.com>
+ */
+
+$object->propertyname $object->methodname([arg, ... ])
